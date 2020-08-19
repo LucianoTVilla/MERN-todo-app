1ca76fc36d71172b3016f1a6ba8fccb739a3fc1c
Luciano Villa <45444670+Surfllux@users.noreply.github.com>
2020-08-19 00:11:10 -0300

    DeleteTodo bug fixed

--- src/components/DeleteTodo.js
+++ src/components/DeleteTodo.js
@@ -1,14 +1,14 @@
 import React, {useEffect} from 'react';
-import { useRouteMatch, useHistory, Link } from 'react-router-dom';
+import { useRouteMatch } from 'react-router-dom';
 
 import { deleteTodo } from '../api'; 
 
 export const DeleteTodo = () => {
-  const match = useRouteMatch()
+  const match = useRouteMatch();
 
-  useEffect( async () => {
-    await deleteTodo(match.params.id)
-  }, []);
+  useEffect(() => {
+    deleteTodo(match.params.id)
+  });
 
   return(
     <div>
